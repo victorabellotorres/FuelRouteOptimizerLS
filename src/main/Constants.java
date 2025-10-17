894d1cdb28298b25d3af8f3b9e36033fc9771b86
Victor Abello <victor.abello@estudiantat.upc.edu>
2025-10-17 18:49:14 +0200

    Constantes annealing añadidas

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -13,5 +13,13 @@ public final class Constants {
     public static int ALGORITMO_ESTADO_INICIAL = 1; // 1: Por orden, 2: Aleatorio, 3: Greedy
     public static int ALGORITMO_BUSQUEDA = 1; // 1: Hill Climbing, 2: Simulated Annealing
     public static int CAMIONES_POR_CENTRO = 1;
+
+    // Simulated annealing parameters
+    public static int SA_NEIGHBORS = 4;
+    public static int SA_STEPS = 10000;
+    public static int SA_STITER = 1;
+    public static int SA_K = 1;
+    public static double SA_LAMBDA = 0.001;
+
 }
 
